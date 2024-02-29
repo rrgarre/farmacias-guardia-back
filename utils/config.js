5e43248a6d895f0fca42ce99df804f69d1af7257
garredev <rrgarredev@gmail.com>
2024-02-29 16:29:25 +0100

    configuracion para server SSL

--- utils/config.js
+++ utils/config.js
@@ -7,9 +7,15 @@ if(process.env.NODE_ENV === "test"){
   const MONGODB_URI = process.env.MONGODB_URI_TEST
 }
 const SECRET_FOR_JWT = process.env.SECRET_FOR_JWT
+const ENABLE_SSL = process.env.RR_ENABLE_SSL
+const SSL_KEY_FILE = process.env.RR_SSL_KEY_FILE
+const SSL_CERT_FILE = process.env.RR_SSL_CERT_FILE
 
 module.exports = {
   PORT,
   MONGODB_URI,
-  SECRET_FOR_JWT
+  SECRET_FOR_JWT,
+  ENABLE_SSL,
+  SSL_KEY_FILE,
+  SSL_CERT_FILE
 }
